6b9ad7514cf25bc02ebb6373bcbe1b9999cb5ddf
abyssowl <70411647+abyssowl@users.noreply.github.com>
2022-09-08 13:30:33 -0500

    Controller Additions
    Added checkout cart with placeholder String, create user account, and post for createKitchen and checkout.

--- src/main/java/com/project/FeastFreedom/controller/FeastFreedomController.java
+++ src/main/java/com/project/FeastFreedom/controller/FeastFreedomController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.FeastFreedom.model.*;
@@ -54,5 +56,28 @@ public class FeastFreedomController {
 	    }   
 	    
 	}
+	
+	@GetMapping("/checkout/{cart}")
+	public ResponseEntity<String> getCart(@PathVariable FeastUser us, @PathVariable String cart){
+		try {
+			/*Cart pulls list of menu items here. String as placeholder, ArrayList<MenuItem> will be the
+			actual variable*/
+	        return new ResponseEntity<String> (cart, HttpStatus.OK);
+	    } 
+	    catch (NoSuchElementException e) {
+	        return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+	    }   
+	}
+	
+	@PostMapping("/checkout")
+	public void createCart(@RequestBody String cart) {
+		//create cart here
+	}
+	
+	@PostMapping("/kitchen")
+	public void createKitchen(@RequestBody Kitchen kit) {
+		kitchenService.insert(kit);
+	}
+	
 	
 }
--- src/main/java/com/project/FeastFreedom/controller/JwtAuthenticationController.java
+++ src/main/java/com/project/FeastFreedom/controller/JwtAuthenticationController.java
@@ -11,14 +11,17 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.FeastFreedom.config.JwtTokenUtil;
+import com.project.FeastFreedom.model.FeastUser;
 import com.project.FeastFreedom.model.JwtRequest;
 import com.project.FeastFreedom.model.JwtResponse;
+import com.project.FeastFreedom.services.UserService;
 
 @RestController
 @CrossOrigin
@@ -32,6 +35,9 @@ public class JwtAuthenticationController {
 
 	@Autowired
 	private UserDetailsService jwtInMemoryUserDetailsService;
+	
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
 	public ResponseEntity<?> generateAuthenticationToken(@RequestBody JwtRequest authenticationRequest)
@@ -57,5 +63,11 @@ public class JwtAuthenticationController {
 		} catch (BadCredentialsException e) {
 			throw new Exception("INVALID_CREDENTIALS", e);
 		}
+	}
+	
+	@PostMapping
+	public void creatUserAccount(@RequestBody FeastUser u) {
+		//Authentication stuff here
+		userService.insert(u);
 	}
 }
