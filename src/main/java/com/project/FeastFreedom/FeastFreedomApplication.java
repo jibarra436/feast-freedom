0d912c39edbb10f4d2a50846324797591f3d18f6
Joey Ibarra <joeyibarra436@gmail.com>
2022-09-12 00:10:35 -0500

    Changed application to servlet, fixed base href, converted to build as .wa

--- src/main/java/com/project/FeastFreedom/FeastFreedomApplication.java
+++ src/main/java/com/project/FeastFreedom/FeastFreedomApplication.java
@@ -2,12 +2,19 @@ package com.project.FeastFreedom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FeastFreedomApplication {
+public class FeastFreedomApplication extends SpringBootServletInitializer  {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FeastFreedomApplication.class, args);
-	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FeastFreedomApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeastFreedomApplication.class);
+    }
 
 }
